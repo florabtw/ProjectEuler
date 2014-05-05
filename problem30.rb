5778d0022849654e2f1d7ca80178b9f2cacb634f
Nick Pierson <nkcrpn@gmail.com>
2014-05-05 12:27:08 -0500

    Solves problem 30

---
+++ problem30.rb
@@ -0,0 +1,25 @@
+def estimate_max(power)
+  number = []
+  loop do
+    number << 9
+    break if num_sum(number.join.to_i, power) <= number.join.to_i
+  end
+  number.join.to_i
+end
+
+def num_sum(n, power)
+  n.to_s.split('').reduce(0) { |sum, c| sum + c.to_i ** power }
+end
+
+def is_match(n, power)
+  num_sum(n, power) == n
+end
+
+power = 5
+max = estimate_max(power)
+nums = []
+(2..max).each do |i|
+  nums << i if is_match(i, power)
+end
+
+puts nums.reduce(:+)
