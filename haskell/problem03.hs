2069c20ccd94530e444742a539fca9c9e52d848e
Nick Pierson <nkcrpn@gmail.com>
2014-08-06 20:34:23 -0500

    Solves problem 3 haskell

---
+++ haskell/problem03.hs
@@ -0,0 +1,10 @@
+import Data.List
+
+main = return $ largestPrimeFactor 600851475143
+
+largestPrimeFactor x =
+    let largest = floor . sqrt $ fromIntegral x
+        smallest = find ((==0) . mod x) [2..largest]
+    in  case smallest of
+            Nothing -> x
+            Just y -> largestPrimeFactor (x `div` y)
