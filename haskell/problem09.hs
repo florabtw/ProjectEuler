0c597fa7a07f2adff24fb34a9cbe25642ba1e369
Nick Pierson <nkcrpn@gmail.com>
2014-08-09 13:56:34 -0500

    Solves problem 9 in haskell

---
+++ haskell/problem09.hs
@@ -0,0 +1,13 @@
+import Control.Monad
+
+main = print . foldl1 (*) $ head triplets
+
+triplets = do
+    x <- [1..]
+    let yMax = (1000-x)
+    y <- [x..yMax]
+    let zMax = min (x+y) (1000-x-y)
+    z <- [y..zMax]
+    guard (x + y + z == 1000)
+    guard (x^2 + y^2 == z^2)
+    return [x,y,z]
